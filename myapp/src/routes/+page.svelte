fe4753cf652fb015685ace976d455683f59c091d
Safwan Rahman <Safwanrahman9@gmail.com>
2023-05-30 16:15:49 -0600

    First commit

---
+++ myapp/src/routes/+page.svelte
@@ -0,0 +1,30 @@
+    <script>
+        let c = document.getElementById("battle-ground");
+        
+
+    
+    </script>
+  
+  
+  
+  <div>
+
+    
+
+    <canvas id="battle-ground" class="battle-ground" height="600px" width="600px"></canvas>
+
+    <button class="button-1"></button>
+    <button></button>
+    <button></button>
+
+
+  </div>
+  
+  <style>
+    .battle-ground{
+        border: 3px solid black;
+        display: block;
+    }
+    
+    
+  </style>
